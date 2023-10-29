0b03a8f352b686a6cf5428b3365af1a0531a408a
noookia8110 <matchman7980@gmail.com>
2023-10-29 20:54:52 +0800

    123

---
+++ Prime/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Prime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(59, 52);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(324, 312);
+            this.checkedListBox1.TabIndex = 2;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label1.Location = new System.Drawing.Point(57, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "請勾選列表中的質數";
+            this.label1.Visible = true;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 387);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "s";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Prime/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prime
+{
+    public partial class Form1 : Form
+    {
+        public List<bool> prime = new List<bool>();
+        public Form1()
+        {
+            string inputFile = "Number.txt";
+            System.IO.StreamReader inputData = new System.IO.StreamReader(inputFile);
+            InitializeComponent();
+            string line = "";
+            line = inputData.ReadLine();
+            int index = 0;
+            BuildPrimeList(10000000);
+            while (line != null)
+            {
+                // dataGridView1.Rows.Add("");
+                checkedListBox1.Items.Add(line);
+                // dataGridView1.Rows[index].Cells[0].Value = line;
+                if (isPrime(int.Parse(line)))
+                {
+                    // dataGridView1.Rows[index].Cells[1].Value = "是";
+                    checkedListBox1.SetItemChecked(index, true);
+                }
+                else
+                {
+                    // dataGridView1.Rows[index].Cells[1].Value = "否";
+                    checkedListBox1.SetItemChecked(index, false);
+                }
+
+                line = inputData.ReadLine();
+                index++;
+            }
+        }
+
+        public void BuildPrimeList(int limit)
+        {
+            bool[] is_prime = new bool[limit+1];
+            for (int i = 0; i < limit+1; i++)
+                is_prime[i] = true;
+            is_prime[0] = false;
+            is_prime[1] = false;
+            for(int i = 0; i < limit + 1; i++)
+            {
+                if (is_prime[i])
+                {
+                    for(int j = 2*i; j < limit; j+=i)
+                    {
+                        is_prime[j] = false;
+                    }
+                }
+            }
+            prime = is_prime.ToList<bool>();
+        }
+
+        public bool isPrime(int num) 
+        {
+            return prime[num];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
